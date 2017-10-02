9e093324f9a295cdde2f666654ee12ecbf35627a
jyoung <sosh9898@naver.com>
2017-10-02 16:35:06 +0900

    first_xml commit

---
+++ app/src/main/java/dct/com/everyfoody/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package dct.com.everyfoody.ui.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dct.com.everyfoody.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
