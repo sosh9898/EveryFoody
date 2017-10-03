5f850fdcf3f894d2f88e10cd19b3be8c7aa2186e
jyoung <sosh9898@naver.com>
2017-10-03 23:47:20 +0900

    notify, register xml apply

---
+++ app/src/main/java/dct/com/everyfoody/ui/home/owner/register/TruckRegisterActivity.java
@@ -0,0 +1,15 @@
+package dct.com.everyfoody.ui.home.owner.register;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dct.com.everyfoody.R;
+
+public class TruckRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_truck_register);
+    }
+}
