650ac029efba5bcf48166211bc78e610453709d5
jyoung <sosh9898@naver.com>
2017-10-04 01:12:28 +0900

    reservation, turn xml apply

---
+++ app/src/main/java/dct/com/everyfoody/ui/home/owner/turn/TurnActivity.java
@@ -0,0 +1,15 @@
+package dct.com.everyfoody.ui.home.owner.turn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dct.com.everyfoody.R;
+
+public class TurnActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_turn);
+    }
+}
---
+++ app/src/main/java/dct/com/everyfoody/ui/reservation/ReservationActivity.java
@@ -0,0 +1,15 @@
+package dct.com.everyfoody.ui.reservation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dct.com.everyfoody.R;
+
+public class ReservationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation);
+    }
+}
