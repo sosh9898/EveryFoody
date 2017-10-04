c083521d810e8817af49fa68312fa249a0d16c71
jyoung <sosh9898@naver.com>
2017-10-04 21:55:55 +0900

    stack init

---
+++ app/src/main/java/dct/com/everyfoody/ui/detail/normal/NormalFragment.java
@@ -0,0 +1,40 @@
+package dct.com.everyfoody.ui.detail.normal;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import butterknife.ButterKnife;
+import dct.com.everyfoody.R;
+
+/**
+ * Created by jyoung on 2017. 10. 4..
+ */
+
+public class NormalFragment extends Fragment {
+
+    public NormalFragment() {
+    }
+
+    public static NormalFragment getInstance(){
+        return new NormalFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_normal_info, null);
+        ButterKnife.bind(this, view);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+}
---
+++ app/src/main/java/dct/com/everyfoody/ui/home/user/TruckRecyclerAdapter.java
@@ -0,0 +1,27 @@
+package dct.com.everyfoody.ui.home.user;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by jyoung on 2017. 10. 4..
+ */
+
+public class TruckRecyclerAdapter extends RecyclerView.Adapter {
+
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
