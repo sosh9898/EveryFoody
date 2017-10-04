0f0645fbd885efcea694f0ed5843e5f0e1dfba15
forgreen0624 <forgreen0624@naver.com>
2017-10-04 17:02:26 +0900

    create ApplicationController

---
+++ app/src/main/java/dct/com/everyfoody/ui/global/ApplicationController.java
@@ -0,0 +1,18 @@
+package dct.com.everyfoody.ui.global;
+
+import android.app.Application;
+
+/**
+ * Created by LEECM on 2017-10-04.
+ */
+
+
+public class ApplicationController extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
+
